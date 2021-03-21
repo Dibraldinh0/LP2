501963985e1226911d976ac7a417509888eb9fba
Dibraldinh0 <79485680+Dibraldinh0@users.noreply.github.com>
2021-03-21 20:14:08 -0300

    Add files via upload

--- modulo2/03 - Pacotes e Visibilidade/3.2/PackApp.java
+++ modulo2/03 - Pacotes e Visibilidade/3.2/PackApp.java
@@ -1,1 +1,41 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
+import figures.*;
+
+class PackApp {
+    public static void main (String[] args) {
+        PackFrame frame = new PackFrame();
+        frame.setVisible(true);
+    }
+}
+
+class PackFrame extends JFrame {
+    Rect r1;
+    Ellipse e1;
+    Losango l1;
+
+    PackFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        this.setTitle("Java Packages");
+        this.setSize(480, 480);
+        this.r1 = new Rect(50, 50, 100, 50, Color.red, Color.blue);
+        this.e1 = new Ellipse(50, 110, 100, 100, Color.green, Color.yellow);
+        this.l1 = new Losango(240, -50, 220, 220, Color.black, Color.white);
+        
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        this.r1.paint(g);
+        this.e1.paint(g);
+        this.l1.paint(g);
+    }
+}
