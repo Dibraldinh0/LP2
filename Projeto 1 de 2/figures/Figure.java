70b2c82bdcdff60852ebff22a52a69f1e3028296
Dibraldinh0 <79485680+Dibraldinh0@users.noreply.github.com>
2021-05-20 22:24:12 -0300

    Update Figure.java

--- Projeto 1 de 2/figures/Figure.java
+++ Projeto 1 de 2/figures/Figure.java
@@ -2,9 +2,34 @@ package figures;
 
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Graphics2D;
 
 public abstract class Figure {
 	public int x, y, w, h;
 	public Color contorno, fundo;
+	
     public abstract void paint (Graphics g);
+    
+    public Figure(int x, int y, int w, int h, Color contorno, Color fundo) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.contorno = contorno;
+		this.fundo = fundo;
+    }
+    
+	public Color borda = new Color(255,10,10);
+	
+	
+	
+	public boolean clicked (int posX, int posY) {
+		  return (((x <= posX) && (y <= posY)) && ((posX <= (x + w)) && (posY <= (y + h))));
+	}
+	
+	public void drawBorder(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(borda);
+		g2d.drawRect(this.x, this.y, this.w, this.h);
+	}	
 }
