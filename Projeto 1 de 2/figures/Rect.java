dbad22636c30dda52e2e5f07d06658217edfc6e7
Dibraldinh0 <79485680+Dibraldinh0@users.noreply.github.com>
2021-05-22 23:27:49 -0300

    Update Rect.java

--- Projeto 1 de 2/figures/Rect.java
+++ Projeto 1 de 2/figures/Rect.java
@@ -5,16 +5,8 @@ import java.awt.Color;
 
 public class Rect extends Figure {
 
-	public int x, y, w, h;
-	private Color contorno, fundo;
-
     public Rect (int x, int y, int w, int h, Color contorno, Color fundo) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.contorno = contorno;
-	 	this.fundo = fundo;
+		super(x, y, w, h , contorno, fundo);
     }
 
 	@Override
@@ -23,6 +15,6 @@ public class Rect extends Figure {
 		g2d.setColor(this.contorno);
         g2d.drawRect(this.x, this.y, this.w, this.h);	
         g2d.setColor(this.fundo);
-		g2d.fillRect(x+1, y+1, w-1, h-1);
+		g2d.fillRect(x, y, w, h);
 	}
 }
