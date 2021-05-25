f2df6a56af38a5d113ad66d5be559ff3121a58fe
Dibraldinh0 <79485680+Dibraldinh0@users.noreply.github.com>
2021-05-24 21:30:18 -0300

    Add files via upload

--- Modulo 3/3.1/figures/Figure.java
+++ Modulo 3/3.1/figures/Figure.java
@@ -1,1 +1,35 @@
+package figures;
 
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import ivisible.IVisible;
+
+public abstract class Figure implements IVisible{
+	public int x, y, w, h;
+	public Color contorno, fundo;
+	
+    //public abstract void paint (Graphics g);
+    
+    public Figure(int x, int y, int w, int h, Color contorno, Color fundo) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.contorno = contorno;
+		this.fundo = fundo;
+    }
+    
+	public Color borda = new Color(255,10,10);	
+	
+	public boolean clicked (int posX, int posY) {
+		  return (((x <= posX) && (y <= posY)) && ((posX <= (x + w)) && (posY <= (y + h))));
+	}
+	
+	public void drawBorder(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(borda);
+		g2d.drawRect(this.x, this.y, this.w, this.h);
+	}	
+}
