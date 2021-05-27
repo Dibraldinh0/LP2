df7b534ab9c122a248033e90ee253736fa6cdb03
Dibraldinh0 <79485680+Dibraldinh0@users.noreply.github.com>
2021-05-26 21:39:50 -0300

    Add files via upload

---
+++ Modulo 4/1/figures/Ellipse.java
@@ -0,0 +1,22 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.Color;
+
+public class Ellipse extends Figure {
+   	
+    public Ellipse (int x, int y, int w, int h, Color contorno, Color fundo) {
+        super(x, y, w, h , contorno, fundo);
+    }
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(this.contorno);
+        g2d.draw(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+        g2d.setColor(this.fundo);
+		g2d.fillOval(x, y, w, h);
+	}
+    
+}
