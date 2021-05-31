78f499c96f974f285bdd311dd1994e3b6ffc7484
Dibraldinh0 <79485680+Dibraldinh0@users.noreply.github.com>
2021-05-31 20:56:47 -0300

    Update Pentagon.java

--- Projeto 1 de 2/figures/Pentagon.java
+++ Projeto 1 de 2/figures/Pentagon.java
@@ -1,35 +1,24 @@
 package figures;
 
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.Color;
 
 public class Pentagon extends Figure {
-
-	private Polygon p = new Polygon();
-
+		
 	public Pentagon (int x, int y, int w, int h, Color contorno, Color fundo) {
-		super(x, y, w, h , contorno, fundo);
-        
-        p.npoints = 4;
-        p.xpoints[0] = (int) (x + w/2);
-        p.xpoints[1] = x + w;
-        p.xpoints[2] = (int) (x + w*0.75);
-        p.xpoints[3] = (int) (x + w*0.25);    
-        
-        p.ypoints[0] = y;
-        p.ypoints[1] = (int) (y + h*0.40);
-        p.ypoints[2] = y+h;
-        p.ypoints[3] = y+h;
-        
-        p.addPoint(x, (int) (y + h*0.40));
-    }
-
-	@Override
+ 		super(x, y, w, h , contorno, fundo);	 	
+	}
+	
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(contorno);
-        g2d.drawPolygon(p.xpoints, p.ypoints, p.npoints);
-        g2d.setColor(fundo);
-        g2d.fillPolygon(p);
+		int[] xdir = {(int) (this.x + this.w/2), this.x + this.w, (int) (this.x + this.w*0.75), (int) (this.x + this.w*0.25), this.x};
+		int[] ydir = {this.y, (int) (this.y + this.h*0.40), this.y+this.h, this.y+this.h, (int) (this.y + this.h*0.40) };
+		g2d.drawPolygon(xdir, ydir, 5); 
+		g2d.setColor(this.contorno);
+		g2d.drawPolygon(xdir, ydir, 5);
+		g2d.setColor(this.fundo);
+		g2d.fillPolygon(xdir, ydir, 5);		
 	}
-
 }
