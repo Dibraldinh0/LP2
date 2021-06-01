bc97ea7373a34a93808e757db55948b08a4f89f3
Dibraldinh0 <79485680+Dibraldinh0@users.noreply.github.com>
2021-05-31 21:23:56 -0300

    Add files via upload

--- Projeto 2 de 2/buttons/Button.java
+++ Projeto 2 de 2/buttons/Button.java
@@ -1,22 +1,16 @@
 package buttons;
+
 import java.awt.*;
 import ivisible.IVisible;
 import figures.Figure;
 
 public class Button implements IVisible {
-    /*static int SPC = 20;
-    static int DIM = 40;
-    static int PAD = 4;*/
     public int idx;
     public Figure figo;
 
     public Button (int idx, Figure figo) {
 		this.idx = idx;
         this.figo = figo;
-        /*this.figo.x = PAD+SPC;
-        this.figo.y = PAD+SPC + idx*DIM;
-        this.figo.w = DIM-PAD*2;
-        this.figo.h = DIM-PAD*2;*/
     }
 	
 	@Override
@@ -30,7 +24,7 @@ public class Button implements IVisible {
         g2d.setColor(Color.BLACK);
         g2d.fillRect(figo.x-5, figo.y-5, figo.w+10, figo.h+10);		
 		if (focused) {
-        	g2d.setColor(Color.gray);
+        	g2d.setColor(Color.GRAY);
         } 
         else {
         	g2d.setColor(Color.LIGHT_GRAY);
