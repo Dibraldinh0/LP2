973d4c4b204d19568f8bd45810d33e86b4bdfbcf
Dibraldinh0 <79485680+Dibraldinh0@users.noreply.github.com>
2021-05-31 21:21:35 -0300

    Add files via upload

--- Projeto 2 de 2/ListApp.java
+++ Projeto 2 de 2/ListApp.java
@@ -169,7 +169,31 @@ class ListFrame extends JFrame {
                     else if (e.getKeyChar() == 'p') {
                     	figs.add(new Pentagon(x, y, w, h, contorno, fundo));
                     	focus = figs.get(figs.size()-1);
+                    }                    
+                    
+                    //Redimensionamento das figuras                    
+                    else if (e.getKeyChar() == 'd' ) {//aumenta largura w da figura						
+						focus.w += 10; 
+						focus = figs.get(figs.size()-1);
                     }
+                    else if (e.getKeyChar() == 'a' ) {//diminui largura w da figura                    	
+                    	focus.w -= 10;
+                    	if (focus.w < 10) {
+							focus.w += 10;
+                    	} 
+						focus = figs.get(figs.size()-1);
+                    }
+                    else if (e.getKeyChar() == 's' ) {//aumenta altura h da figura						
+						focus.h += 10; 
+						focus = figs.get(figs.size()-1);
+                    }
+                    else if (e.getKeyChar() == 'w' ) {//diminui altura h da figura						
+						focus.h -= 10;
+						if (focus.h < 10) {
+							focus.h += 10;
+                    	}  
+						focus = figs.get(figs.size()-1);
+                    }                    
                     
                     //Troca de cores
                     else if (e.getKeyChar() == 'c'){
@@ -185,29 +209,7 @@ class ListFrame extends JFrame {
                     		contFundo = 0;
                     	}
                     	focus.fundo = cores[contFundo];
-                    }                                       
-                    
-                    //Redimensionamento das figuras                    
-                    else if (e.getKeyChar() == 'd' ) {
-						//aumenta largura w da figura
-						focus.w += 10; 
-						focus = figs.get(figs.size()-1);
-                    }
-                    else if (e.getKeyChar() == 'a' ) {
-                    	//diminui largura w da figura
-                    	focus.w -= 10; 
-						focus = figs.get(figs.size()-1);
-                    }
-                    else if (e.getKeyChar() == 's' ) {
-						//aumenta altura h da figura
-						focus.h += 10; 
-						focus = figs.get(figs.size()-1);
-                    }
-                    else if (e.getKeyChar() == 'w' ) {
-						//diminui altura h da figura
-						focus.h -= 10; 
-						focus = figs.get(figs.size()-1);
-                    }
+                    }                    
                     
                     //Deletar a figura em foco
                     else if (e.getKeyCode() == 127) {
